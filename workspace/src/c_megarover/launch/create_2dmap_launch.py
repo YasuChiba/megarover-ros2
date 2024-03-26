ada4b141f7023dba14c5e783abc75b40e1ec3191
YasuChiba <yasu.bv0309@gmail.com>
2024-03-27 01:56:30 +0900

    update

--- workspace/src/c_megarover/launch/create_2dmap_launch.py
+++ workspace/src/c_megarover/launch/create_2dmap_launch.py
@@ -5,6 +5,9 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 import launch
 from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 ################### user configure parameters for ros2 start ###################
 xfer_format   = 0    # 0-Pointcloud2(PointXYZRTL), 1-customized pointcloud format
@@ -37,13 +40,26 @@ livox_ros2_params = [
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
 
+    rviz_use = LaunchConfiguration('rviz')
+    simulator_use = LaunchConfiguration('simulator')
+
+    declare_rviz_cmd = DeclareLaunchArgument(
+        'rviz', default_value='true',
+        description='Use RViz to monitor results'
+    )
+
+    declare_simulator_cmd = DeclareLaunchArgument(
+        'simulator', default_value='false',
+        description='Use Simulator and do not use Livox LiDARs'
+    )
 
     livox_driver = Node(
         package='livox_ros_driver2',
         executable='livox_ros_driver2_node',
         name='livox_lidar_publisher',
         output='screen',
-        parameters=livox_ros2_params
+        parameters=livox_ros2_params,
+        condition=UnlessCondition(simulator_use)
         )
 
     # create pointcloud_to_laserscan Node
@@ -57,7 +73,7 @@ def generate_launch_description():
             'target_frame': '',
             'transform_tolerance': 0.01,
             'min_height': 0.0,
-            'max_height': 0.5,
+            'max_height': 1.0,
             'angle_min': -3.1415,  # -M_PI/2
             'angle_max': 3.1415,  # M_PI/2
             'angle_increment': 0.0087,  # M_PI/360.0
@@ -79,16 +95,24 @@ def generate_launch_description():
         description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
 
     #slam_toolboxの起動設定
-    start_async_slam_toolbox_node = Node(
-        parameters=[
-          slam_params_file,
-          {'use_sim_time': use_sim_time}
-        ],
-        package='slam_toolbox',
-        executable='async_slam_toolbox_node',
-        name='slam_toolbox',
-        output='screen')
+    #start_async_slam_toolbox_node = Node(
+    #    parameters=[
+    #      slam_params_file,
+    #      {'use_sim_time': use_sim_time}
+    #    ],
+    #    package='slam_toolbox',
+    #    executable='sync_slam_toolbox_node',
+    #    name='slam_toolbox',
+    #    output='screen')
 
+    slam_toolbox = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [os.path.join(get_package_share_directory('slam_toolbox'), 'launch', 'online_sync_launch.py')]
+            ),
+            launch_arguments=[
+                ('slam_params_file', slam_params_file),
+                ('use_sim_time', use_sim_time),
+            ])
 
     # add static_transform_publisher.
     static_transform_publisher1 = Node(
@@ -98,6 +122,13 @@ def generate_launch_description():
         output='screen'
     )
 
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        condition=IfCondition(rviz_use),
+        arguments=['-d', os.path.join(get_package_share_directory('c_megarover'), 'config', '2dslam.rviz')],
+    )
+
     
 
     return LaunchDescription([
@@ -105,7 +136,10 @@ def generate_launch_description():
         livox_driver,
         pointcloud_to_laserscan,
         declare_slam_params_file_cmd,
-        start_async_slam_toolbox_node,
+        slam_toolbox,
+        rviz_node,
+        declare_simulator_cmd,
+        declare_rviz_cmd
         # launch.actions.RegisterEventHandler(
         #     event_handler=launch.event_handlers.OnProcessExit(
         #         target_action=livox_rviz,
