4772dd1ed477ef45b8f5685f4b98327c8141c2ec
YasuChiba <yasu.bv0309@gmail.com>
2024-04-16 17:09:36 +0900

    update

--- start_navigation.sh
+++ start_navigation.sh
@@ -23,7 +23,7 @@ docker exec megarover-ros2-ros-1 /bin/bash -c "source /opt/ros/humble/setup.bash
                                                 source /home/user/uros_ws/install/setup.bash && \
                                                 source /home/user/workspace/install/setup.bash && \
                                                 if [ -e /dev/ttyUSB0 ]; then sudo chmod 666 /dev/ttyUSB0; fi && \
-                                                ros2 launch c_megarover navigation_launch.py simulator:=true rviz:=false map_file_path:=/home/user/workspace/pcd/sendagi.pcd map_2d_file_path:=/home/user/workspace/map/sendagi.yaml"
+                                                ros2 launch c_megarover navigation_launch.py simulator:=true rviz:=false map_file_path:=/home/user/workspace/maps/sendagi.pcd map_2d_file_path:=/home/user/workspace/maps/sendagi.yaml"
 
 echo "ROS2 nodes have been launched."
 
