a33624bd637223c23cf806b6eb0316075fab6f2f
YasuChiba <yasu.bv0309@gmail.com>
2024-05-07 15:45:47 +0900

    update

--- start_navigation.sh
+++ start_navigation.sh
@@ -6,14 +6,15 @@ CONTAINER_NAME="megarover-start-recording"
 echo "Starting ROS 2 bag recording..."
 
 # Step 1: Run Docker container in detached mode and save the container ID
-docker run -it --privileged --net=host --ipc=host --pid=host \
+docker run -it --runtime=nvidia --network=host --ipc=host --pid=host --privileged \
         -v $(pwd)/workspace:/home/user/workspace \
-        -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -v /dev:/dev \
         -e DISPLAY=$DISPLAY \
-        --name $CONTAINER_NAME \
+        --name $CONTAINER_NAME \ 
         megarover-ros2-ros \
         /home/user/workspace/container_entrypoints/start_3d_navigation.sh
-
+        
 docker stop $CONTAINER_NAME
 
 # Optional: Wait a bit if you need to ensure that logs or other operations complete
